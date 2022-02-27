510e627085fd66a02aff1029f4f6ad07240a1a38
bmolyneaux <bmolynea@gmail.com>
2022-02-26 16:00:43 -0800

    war3 model icon

--- plugin.gd
+++ plugin.gd
@@ -8,8 +8,10 @@ var import_plugin
 func _enter_tree():
 	import_plugin = preload("import_plugin.gd").new()
 	add_import_plugin(import_plugin)
+	add_custom_type("UnitModel", "Spatial", preload("./scripts/UnitModel.gd"), preload("./war3_icon.png"))
 
 
 func _exit_tree():
 	remove_import_plugin(import_plugin)
 	import_plugin = null
+	remove_custom_type("War3Model")
